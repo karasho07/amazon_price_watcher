0c74a6c5fea8b4cc5d0092689d346042c83e30d3
karasho07 <karasho07@gmail.com>
2025-05-06 14:44:40 +0900

    app.config修正

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
